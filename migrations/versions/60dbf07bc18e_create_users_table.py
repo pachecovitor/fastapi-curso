3f96568bf7c8ea5e56d37883ebf61ce68ffca26a
Vitor Pacheco <vitorpacheco2000@hotmail.com>
2024-01-06 20:39:59 -0300

    Atualizando endpoints para usar o banco de dados real

--- migrations/versions/60dbf07bc18e_create_users_table.py
+++ migrations/versions/60dbf07bc18e_create_users_table.py
@@ -7,9 +7,8 @@ Create Date: 2024-01-06 20:27:56.381197
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '60dbf07bc18e'
